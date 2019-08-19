4a935dda24a9e1ca32d679d0150714f0b4e11cff
ziqiyap <yapziqi24@gmail.com>
2019-08-19 11:14:59 +0800

    added demo files

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include "demo.h"
+
+/**
+ *  @brief  Prints "Hello world"
+ *  @author author
+ */
+void printHello()
+{
+	printf("Hello World \r\n");
+    return;
+}
---
+++ demo.h
@@ -0,0 +1,16 @@
+
+#include <string>
+
+/** Simple module for doxygen documentation demo
+ *  @brief  Prints hello world
+ *
+ *  Example:
+ *  @code{.cpp}
+ *  #include "demo.h"
+ *
+ *  printHello();
+ *  
+ *  @endcode
+ */
+
+void printHello();
